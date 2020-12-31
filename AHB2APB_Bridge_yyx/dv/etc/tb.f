456bfaa33488aca5830bb627d9aa2cab1b8208b4
Yunxiao Yang <yyunxiao@marvell.com>
2020-12-31 16:15:15 +0800

    Add makefile and test list

---
+++ AHB2APB_Bridge_yyx/dv/etc/tb.f
@@ -0,0 +1,16 @@
+-timescale=1ns/1ns/1n
+
++incdir+$DV_ROOT/env
++incdir+$DV_ROOT/vip/ahbl_mst
++incdir+$DV_ROOT/vip/apb_slv
+
+//Source file
+$DV_ROOT/vip/ahbl_mst/ahbl_mst_agt.svh
+$DV_ROOT/vip/apb_slv/apb_slv_agt.svh
+$DV_ROOT/env/ahb2apb_pkg.sv
+$DV_ROOT/env/ahb2apb_base_test.sv
+$DV_ROOT/tb/reset_if.sv
+$DV_ROOT/tb/ahb2apb_tb.sv
+
+
+
